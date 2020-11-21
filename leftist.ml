364213ccc15b489ebd682f74ca79ccf982775dd0
WPaupa <wosio02@gmail.com>
2020-11-21 23:09:45 +0100

    Update leftist.ml

--- leftist.ml
+++ leftist.ml
@@ -1,7 +1,12 @@
+(*DRZEWA LEWICOWE - WPF ZAD 2*)
+(*AUTOR KODU - WOJCIECH PAUPA*)
+(*CODE REVIEW - KACPER HARASIMOWICZ*)
+(*pierwszy element Node to lewe poddrzewo, drugi to prawe, trzeci to klucz, czwarty to prawa wysokość*)
 type 'a queue =
   | Null
   | Node of 'a queue * 'a queue * 'a * int;; 
 
+(*zwraca prawą wysokość, tylko null się liczy jako liść*)
 let sval a =
   match a with
   | Null -> 0
@@ -12,8 +17,11 @@ let rec join a b =
   | Null,b->b
   | a,Null->a
   | Node(al,ar,akey,asval), Node(bl,br,bkey,bsval) ->
+      (*jeśli klucz a>klucz b, to wykonujemy ten sam algorytm, ale z zamienionymi a i b*)
       if akey <= bkey then
+        (*złączamy prawe poddrzewo z drzewem do złączenia rekurencyjnie*)
         let r = join ar b 
+        (*zamieniamy kolejność lewego z prawym tak, żeby spełniony był warunek lewicowości*))
         in if al = Null then
           Node(r,al,akey,1)
         else if (sval r) <= (sval al)
@@ -24,8 +32,6 @@ let rec join a b =
           Node(r,bl,bkey,1)
         else if (sval r) <= (sval bl)
         then Node(bl,r,bkey,1+(sval r)) else Node(r,bl,bkey,1+(sval bl));;
-
-let l n = Node(Null,Null,n,1);;
 
 let empty = Null;;
 let add a q = join q (l a);;
