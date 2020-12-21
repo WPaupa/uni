429f7ffe1004b2b276a03a236c4a89d31ea7b1a4
WPaupa <wosio02@gmail.com>
2020-12-21 16:46:15 +0100

    topol+j

--- origami.ml
+++ origami.ml
@@ -1,7 +1,7 @@
 type point = float * float;;
 type kartka = point -> int;; 
 
-let eps = 0.0001;;
+let eps = 0.0000001;;
 let (<=) x y = if (x)<=(y+.eps) then true else false;; 
 let (=.) x y = if (x <= y) && (y <= x) then true else false;;
 
@@ -9,20 +9,15 @@ let prostokat (x1,y1) (x2,y2) (x,y) =
   if ((x1<=x) && (x<=x2)) && ((y1<=y) && (y<=y2)) then 1 else 0;;
 
 let kolko (xs,ys) r (x,y) =
-  if r>1e150 then 
-    let xs = xs /. r and ys = ys /. r and x = x /. r and y = y /. r in 
-    if ((x-.xs)*.(x-.xs)) +. ((y-.ys)*.(y-.ys)) <= 1. then 1 else 0
-  else if ((x-.xs)*.(x-.xs)) +. ((y-.ys)*.(y-.ys)) <= r *. r then 1 else 0 ;;
+  if ((x-.xs)*.(x-.xs)) +. ((y-.ys)*.(y-.ys)) <= r *. r then 1 else 0 ;;
 
 let cross (ux,uy) (vx,vy) = (uy*.vx)-.(ux*.vy);;
-let scalar (ux,uy) (vx,vy) = (ux*.vx)+.(uy*.vy);; 
+let dot (ux,uy) (vx,vy) = (ux*.vx)+.(uy*.vy);; 
 let mult alfa (ux,uy) = (alfa*.ux,alfa*.uy);;
 let len (ux,uy) = sqrt((ux*.ux)+.(uy*.uy));; 
 let vec (x1,y1) (x2,y2) = (x2-.x1,y2-.y1);;
 let vers p1 p2 = mult (1./.(len (vec p1 p2))) (vec p1 p2) ;;
 let plus (ux,uy) (vx,vy) = ((ux+.vx),(uy+.vy));;
-let minus u v = plus u (mult (-1.) v);;
-let normal (vx,vy) = (vy,~-.vx);; 
 
 
 let rec zloz p1 p2 f p = 
@@ -32,7 +27,7 @@ let rec zloz p1 p2 f p =
   then 0
   else 
     let n = vers p1 p2 in
-    let v = plus (vec p p1) (mult (-2.*.(scalar (vec p p1) n)) n)
+    let v = plus (vec p p1) (mult (-2.*.(dot (vec p p1) n)) n)
     in (f (plus p1 v))+(f p);; 
 
 let skladaj lst f = List.fold_left (fun f (p1,p2) -> zloz p1 p2 f) f lst;; 
---
+++ topol.ml
@@ -0,0 +1,20 @@
+open PMap;;
+exception Cykliczne;;
+let topol g =
+  let m = List.fold_left (fun m (k,v) -> add k v m) empty g in
+  let marx = ref (create compare)
+  and wyn = ref ([]) in
+  let rec visit n tempmarx =
+    try 
+      if (find n !marx) then ();
+    with Not_found ->
+      try
+        if (find n tempmarx) then raise Cykliczne;
+      with Not_found ->
+        (try
+           List.iter (fun x -> visit x (add n true tempmarx)) (find n m)
+         with Not_found -> ());
+        marx := add n true !marx;
+        wyn := n::(!wyn) in
+  iter (fun k v -> try find k (!marx);() with Not_found -> visit(k) empty) m;
+  !wyn;;
