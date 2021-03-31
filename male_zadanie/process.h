fb43660756f261237495a6ddb71dca002b73dc25
WPaupa <wosio02@gmail.com>
2021-03-31 22:17:17 +0200

    describe ub on unsigned long longs

--- male_zadanie/process.h
+++ male_zadanie/process.h
@@ -6,6 +6,8 @@
 //czy ciag znakow word to liczba, czy nieliczba
 //oraz w jakim systemie jest zapisany.
 //potem dodaje go do *line.
+//liczby poza zakresem traktujemy tak,
+//jakby byly rowne zakresowi (tak jak strtoX)
 void process(char *word, line *line);
 
 #endif //MALE_ZADANIE_PROCESS_H
